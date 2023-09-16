4dd8d2183c9c733e6449150ee6ad19fd266fa750
ShivamPatel2805 <141736755+ShivamPatel2805@users.noreply.github.com>
2023-09-16 13:44:44 +0530

    commit

--- Human_Resources_Management_App-1/src/main/java/com/becoder/controller/UserController.java
+++ Human_Resources_Management_App-1/src/main/java/com/becoder/controller/UserController.java
@@ -19,14 +19,14 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepo;
 
-	@ModelAttribute
+/*	@ModelAttribute
 	private void userDetails(Model m, Principal p) {
 		String email = p.getName();
 		UserDtls user = userRepo.findByEmail(email);
 
 		m.addAttribute("user", user);
 
-	}
+	}*/
 
 	@GetMapping("/")
 	public String home() {
